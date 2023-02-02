839530b2e20805f21921f612ba1e174282be3472
SaadatAliPhd <frhnsaif@yahoo.com>
2023-02-02 23:46:55 +0500

    new

--- pricing-ui/app/page.tsx
+++ pricing-ui/app/page.tsx
@@ -5,8 +5,8 @@ import styles from './page.module.css'
 import { ChakraProvider ,Heading, Text} from '@chakra-ui/react'
 import Practise from './Practise'
 import Header from './Header'
-import Pricing from '@/app/Pricing'
-import Features from '@/app/Features'
+import Pricing from './Pricing'
+import Features from './Features'
 
 const inter = Inter({ subsets: ['latin'] })
 
